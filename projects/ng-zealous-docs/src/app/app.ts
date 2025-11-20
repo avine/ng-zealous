9f4907281e9230801143c5178c0525d23a714a2f
Stéphane Francel <stephane.francel@gmail.com>
2025-11-20 11:08:19 +0100

    docs: add theme-switcher

--- projects/ng-zealous-docs/src/app/app.ts
+++ projects/ng-zealous-docs/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ZThemeModule } from 'ng-zealous/theme';
 import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatIconModule, RouterLink, RouterOutlet],
+  imports: [MatButtonModule, MatIconModule, RouterLink, RouterOutlet, ZThemeModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
